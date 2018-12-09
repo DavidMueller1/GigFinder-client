a3e805305365e75a742b7546fadf43bd08ccce54
muellerda <david.muelle2@web.de>
2018-12-09 18:11:47 +0100

    First attempt to create the Tab Layout we need for the App. Not 100% done yet.

--- app/src/main/java/com/example/david/gigfinder/MainActivity.java
+++ app/src/main/java/com/example/david/gigfinder/MainActivity.java
@@ -1,13 +1,39 @@
 package com.example.david.gigfinder;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    private SectionsPageAdapter sectionsPageAdapter;
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d(TAG, "App starting...");
+
+        sectionsPageAdapter = new SectionsPageAdapter(getSupportFragmentManager());
+
+        mViewPager = findViewById(R.id.container); // TODO ?
+        setupViewPager(mViewPager);
+
+        TabLayout tabLayout = findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
     }
+
+    private void setupViewPager(ViewPager viewPager) {
+        SectionsPageAdapter adapter = new SectionsPageAdapter(getSupportFragmentManager());
+        adapter.addFragment(new ExploreFragment(), getString(R.string.nav_explore));
+        adapter.addFragment(new FavoritesFragment(), getString(R.string.nav_favorites));
+        viewPager.setAdapter(adapter);
+    }
+
+
 }
