9ce771d52fe8fa80151d3611d59942285b51eb24
muellerda <david.muelle2@web.de>
2018-12-14 14:57:18 +0100

    UI für Registrierung very basic artist registration with simple user input check without backend check

--- app/src/main/java/com/example/david/gigfinder/RegistrationArtistActivity.java
+++ app/src/main/java/com/example/david/gigfinder/RegistrationArtistActivity.java
@@ -1,13 +1,101 @@
 package com.example.david.gigfinder;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class RegistrationArtistActivity extends AppCompatActivity {
+    private static final String TAG = "DEBUGLOG_RegistrationArtistActivity";
+
+    EditText nameField;
+    EditText descriptionField;
+    Spinner genreSpinner;
+    EditText backgroundColorPicker; // TODO use color picker
+
+    String artistName;
+    String artistDescription;
+    String artistGenre;
+    int artistBackgroundColor;
+
+    Button registrationButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration_artist);
+
+        nameField = findViewById(R.id.registration_artist_name);
+        descriptionField = findViewById(R.id.registration_artist_description);
+        genreSpinner = findViewById(R.id.registration_artist_genre);
+        backgroundColorPicker = findViewById(R.id.registration_artist_color);
+
+        String[] spinnerItems = new String[]{getResources().getString(R.string.artist_genre_choose), getResources().getString(R.string.artist_genre_techno), getResources().getString(R.string.artist_genre_schlager), getResources().getString(R.string.artist_genre_rock), getResources().getString(R.string.artist_genre_other)};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_dropdown_item, spinnerItems);
+        genreSpinner.setAdapter(adapter);
+
+
+        registrationButton = findViewById(R.id.button_artist_registration);
+        registrationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                performRegistration();
+            }
+        });
+    }
+
+    /**
+     * Called when the user presses the registrate button
+     */
+    private void performRegistration() {
+        Log.d(TAG, "Checking user input...");
+        if(checkUserInputBasic()) {
+            Log.d(TAG, "User input ok");
+            // TODO redirect to tabview and send data to Server
+        }
+    }
+
+    /**
+     * Checks whether the user input is valid (ex. name not empty)
+     */
+    private boolean checkUserInputBasic() {
+        artistName = nameField.getText().toString();
+        if(artistName.equals("")) {
+            Toast.makeText(getApplicationContext(),"Namensfeld ist leer.",Toast.LENGTH_SHORT).show();
+            Log.d(TAG, "Namefield empty.");
+            return false;
+        }
+
+        artistDescription = descriptionField.getText().toString(); // Description is optional
+
+        String genreString = genreSpinner.getSelectedItem().toString();
+        if(genreString.equals(getResources().getString(R.string.artist_genre_choose))) {
+            Toast.makeText(getApplicationContext(),"Kein Genre ausgewählt",Toast.LENGTH_SHORT).show();
+            Log.d(TAG, "No genre selected.");
+            return false;
+        }
+
+        String colorString = backgroundColorPicker.getText().toString();
+        if(!colorString.matches("#(\\d|a|b|c|d|e|f){6}")) {
+            Toast.makeText(getApplicationContext(),"Falsches Farbformat (Bsp. #1a2bc3)",Toast.LENGTH_SHORT).show();
+            Log.d(TAG, "Wrong hex format on color.");
+            return false;
+        }
+        try {
+            artistBackgroundColor = Color.parseColor(colorString);
+        }
+        catch(Exception e) {
+            Toast.makeText(getApplicationContext(),"Fehler beim Farbformat. Bitte andere Farbe wählen.",Toast.LENGTH_SHORT).show();
+            Log.d(TAG, "Error on parsing color.");
+            return false;
+        }
+
+        return true;
     }
 }
--- app/src/main/java/com/example/david/gigfinder/SelectUserActivity.java
+++ app/src/main/java/com/example/david/gigfinder/SelectUserActivity.java
@@ -1,13 +1,51 @@
 package com.example.david.gigfinder;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SelectUserActivity extends AppCompatActivity {
+
+    Button artistButton;
+    Button hostButton;
+
+    String googleID;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_user);
+
+        // TODO Get google ID from Login
+        googleID = "ThisIsATest";
+
+        artistButton = findViewById(R.id.button_artist);
+        artistButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startArtistRegistration();
+            }
+        });
+
+        hostButton = findViewById(R.id.button_location_host);
+        hostButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startHostRegistration();
+            }
+        });
+    }
+
+    private void startArtistRegistration() {
+        Intent intent = new Intent(getApplicationContext(), RegistrationArtistActivity.class);
+        intent.putExtra("googleID", googleID);
+        startActivity(intent);
+    }
+
+    private void startHostRegistration() {
+        // TODO Activity für Registrierung des Hosts aufrufen
     }
 }
