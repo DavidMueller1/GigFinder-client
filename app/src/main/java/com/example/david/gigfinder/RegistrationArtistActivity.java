f82e36046590e2dd2004581c4a7a46f47c33343f
muellerda <david.muelle2@web.de>
2018-12-14 13:23:43 +0100

    UI für Registrierung wip

---
+++ app/src/main/java/com/example/david/gigfinder/RegistrationArtistActivity.java
@@ -0,0 +1,13 @@
+package com.example.david.gigfinder;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class RegistrationArtistActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration_artist);
+    }
+}
