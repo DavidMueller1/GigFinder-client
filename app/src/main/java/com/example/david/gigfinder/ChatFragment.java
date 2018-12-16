0c40aa00c2a7d9f2a52553d57f4fe0c39bcca6ff
HasanTuralic <turalic.ha@gmail.com>
2018-12-16 12:47:56 +0100

    Added chat_row and ChatAdapter to ChatFragment (with placeholders)

--- app/src/main/java/com/example/david/gigfinder/ChatFragment.java
+++ app/src/main/java/com/example/david/gigfinder/ChatFragment.java
@@ -7,13 +7,33 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.example.david.gigfinder.adapters.ChatAdapter;
+
+import java.util.ArrayList;
 
 public class ChatFragment extends Fragment {
+
     private static final String TAG = "APPLOG - ChatFragment";
+    private ChatAdapter chatAdapter;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_chat, container, false);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        ArrayList<String[]> placeholderStrings = new ArrayList<>();
+        placeholderStrings.add(new String[]{"Artist Name", "Placeholder message..."});
+        placeholderStrings.add(new String[]{"Host Name", "Test message..."});
+
+        chatAdapter = new ChatAdapter(this.getContext(), placeholderStrings);
+        ListView listView = (ListView) getView().findViewById(R.id.chatListView);
+        listView.setAdapter(chatAdapter);
     }
 }
