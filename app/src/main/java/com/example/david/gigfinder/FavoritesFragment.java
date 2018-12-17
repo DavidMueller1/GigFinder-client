9be836366e0e0e2e91ebe5a6438032cd0f2acf9d
HasanTuralic <turalic.ha@gmail.com>
2018-12-17 09:22:17 +0100

    ExploreFragment and Favorites

--- app/src/main/java/com/example/david/gigfinder/FavoritesFragment.java
+++ app/src/main/java/com/example/david/gigfinder/FavoritesFragment.java
@@ -3,17 +3,40 @@ package com.example.david.gigfinder;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.example.david.gigfinder.adapters.ChatAdapter;
+import com.example.david.gigfinder.adapters.FavAdapter;
+
+import java.util.ArrayList;
 
 public class FavoritesFragment extends Fragment {
+
     private static final String TAG = "APPLOG - FavoritesFragment";
+    private FavAdapter favAdapter;
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_favorites, container, false);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        ArrayList<String[]> placeholderStrings = new ArrayList<>();
+        placeholderStrings.add(new String[]{"Artist Name", "Placeholder message..."});
+        placeholderStrings.add(new String[]{"Host Name", "Test message..."});
+
+        favAdapter = new FavAdapter(this.getContext(), placeholderStrings);
+        ListView listView = (ListView) getView().findViewById(R.id.favListView);
+        listView.setAdapter(favAdapter);
     }
 }
