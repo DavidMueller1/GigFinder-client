595791aacd8434daf5e2d2db8d7cf7539f110288
HasanTuralic <turacool8>
2018-12-22 12:14:24 +0100

    ChatActivity Transition

--- app/src/main/java/com/example/david/gigfinder/ChatFragment.java
+++ app/src/main/java/com/example/david/gigfinder/ChatFragment.java
@@ -1,13 +1,17 @@
 package com.example.david.gigfinder;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 import com.example.david.gigfinder.adapters.ChatAdapter;
@@ -39,5 +43,15 @@ public class ChatFragment extends Fragment {
         chatAdapter = new ChatAdapter(this.getContext(), placeholderStrings);
         ListView listView = (ListView) getView().findViewById(R.id.chatListView);
         listView.setAdapter(chatAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getActivity(), ChatActivity.class);
+                ActivityOptionsCompat options = ActivityOptionsCompat.
+                        makeSceneTransitionAnimation(getActivity(), chatAdapter.getChatImgs().get(position), "chat");
+                startActivity(intent, options.toBundle());
+            }
+        });
     }
 }
