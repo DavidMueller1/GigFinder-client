895c18c02ecf522f10588171c7536f19bc13cc2c
HasanTuralic <turalic.ha@gmail.com>
2018-12-23 11:33:27 +0100

    ChatActivity layout (almost done)

--- app/src/main/java/com/example/david/gigfinder/ChatActivity.java
+++ app/src/main/java/com/example/david/gigfinder/ChatActivity.java
@@ -2,12 +2,42 @@ package com.example.david.gigfinder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.example.david.gigfinder.adapters.MessageListAdapter;
+import com.example.david.gigfinder.data.Message;
+import com.example.david.gigfinder.data.User;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ChatActivity extends AppCompatActivity {
+
+    private RecyclerView mMessageRecycler;
+    private MessageListAdapter mMessageAdapter;
+    private ArrayList<Message> messageList = new ArrayList<Message>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        User testUser1 = new User();
+        testUser1.setName("Friend");
+        testUser1.setId(2);
+
+        User testUser2 = new User();
+        testUser2.setId(1);
+
+        messageList.add(new Message("Hi how are you? This is just a test Message", testUser2, 1355270400000L));
+        messageList.add(new Message("Reply Test Message",testUser1, 1355270400000L));
+
+        mMessageRecycler = (RecyclerView) findViewById(R.id.reyclerview_message_list);
+        mMessageAdapter = new MessageListAdapter(this, messageList);
+        mMessageRecycler.setLayoutManager(new LinearLayoutManager(this));
+        mMessageRecycler.setAdapter(mMessageAdapter);
+
+        mMessageAdapter.notifyDataSetChanged();
     }
 }
---
+++ app/src/main/java/com/example/david/gigfinder/adapters/MessageListAdapter.java
@@ -0,0 +1,80 @@
+package com.example.david.gigfinder.adapters;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.david.gigfinder.R;
+import com.example.david.gigfinder.ReceivedMsgHolder;
+import com.example.david.gigfinder.SentMsgHolder;
+import com.example.david.gigfinder.data.Message;
+
+import java.util.List;
+
+public class MessageListAdapter extends RecyclerView.Adapter{
+
+    private static final int VIEW_TYPE_MESSAGE_SENT = 1;
+    private static final int VIEW_TYPE_MESSAGE_RECEIVED = 2;
+
+    private Context mContext;
+    private List<Message> mMessageList;
+
+    public MessageListAdapter(Context context, List<Message> messageList) {
+        mContext = context;
+        mMessageList = messageList;
+    }
+
+    @Override
+    public int getItemCount() {
+        return mMessageList.size();
+    }
+
+    // Determines the appropriate ViewType according to the sender of the message.
+    @Override
+    public int getItemViewType(int position) {
+        Message message = (Message) mMessageList.get(position);
+
+        if (message.getSender().getId() == 1) //TODO thisUser.id anstatt 1
+            {
+            // If the current user is the sender of the message
+            return VIEW_TYPE_MESSAGE_SENT;
+        } else {
+            // If some other user sent the message
+            return VIEW_TYPE_MESSAGE_RECEIVED;
+        }
+    }
+
+    // Inflates the appropriate layout according to the ViewType.
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view;
+
+        if (viewType == VIEW_TYPE_MESSAGE_SENT) {
+            view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.item_msg_sent, parent, false);
+            return new SentMsgHolder(view);
+        } else if (viewType == VIEW_TYPE_MESSAGE_RECEIVED) {
+            view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.item_msg_received, parent, false);
+            return new ReceivedMsgHolder(view);
+        }
+
+        return null;
+    }
+
+    // Passes the message object to a ViewHolder so that the contents can be bound to UI.
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        Message message = (Message) mMessageList.get(position);
+
+        switch (holder.getItemViewType()) {
+            case VIEW_TYPE_MESSAGE_SENT:
+                ((SentMsgHolder) holder).bind(message);
+                break;
+            case VIEW_TYPE_MESSAGE_RECEIVED:
+                ((ReceivedMsgHolder) holder).bind(message);
+        }
+    }
+}
