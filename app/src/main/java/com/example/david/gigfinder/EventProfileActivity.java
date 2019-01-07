a2d66a32d826b5e9d3281dd0d268e8db6ed7bdf5
muellerda <david.muelle2@web.de>
2019-01-07 04:27:29 +0100

    Event has now a clickable Event Location (Location is stored as Place). When clicked it is opened in Google Maps

--- app/src/main/java/com/example/david/gigfinder/EventProfileActivity.java
+++ app/src/main/java/com/example/david/gigfinder/EventProfileActivity.java
@@ -1,6 +1,8 @@
 package com.example.david.gigfinder;
 
+import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
 import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,12 +15,21 @@ import com.example.david.gigfinder.data.Artist;
 import com.example.david.gigfinder.data.Event;
 import com.example.david.gigfinder.data.Host;
 import com.example.david.gigfinder.data.enums.Genre;
+import com.google.android.gms.location.places.GeoDataClient;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.PlaceBufferResponse;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.maps.model.RuntimeRemoteException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 import java.sql.Timestamp;
 import java.util.ArrayList;
 
 public class EventProfileActivity extends AppCompatActivity {
     private static final String TAG = "EventProfileActivity";
+
+    private Event event;
 
     TextView titleText;
     TextView genreText;
@@ -32,6 +43,8 @@ public class EventProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_profile);
+
+
 
         titleText = findViewById(R.id.event_title);
         genreText = findViewById(R.id.event_genre);
@@ -47,23 +60,56 @@ public class EventProfileActivity extends AppCompatActivity {
             }
         });
 
-        // Test Event
-        ArrayList<Genre> list = new ArrayList<>();
-        list.add(Genre.ROCK);
-        list.add(Genre.HOUSE);
-        Event testEvent = new Event(1, "Testevent in der gemütlichen Beispielbar",
-                "Suche talentierten Drehorgelspieler für Freitag Abend in meiner Bar. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.", list,
-                new Timestamp(2019, 1, 20, 20, 15, 0, 0),
-                new Timestamp(2019, 1, 21, 3, 30, 0, 0), null);
+        // TODO move the following code (including the callback method) to where the Event is generated
 
-        displayEvent(testEvent);
+        // region Test Event
+        GeoDataClient mGeoDataClient = Places.getGeoDataClient(this);
+        Task<PlaceBufferResponse> placeResult = mGeoDataClient.getPlaceById("ChIJQwJTzpl1nkcR2vIR4mH1Bfw");
+        placeResult.addOnCompleteListener(mUpdatePlaceDetailsCallback);
+
     }
 
     /**
-     * Displays the given Event in the Activity
-     * @param event
+     *  Callback for after the Place is found by its ID
      */
-    private void displayEvent(Event event) {
+    private OnCompleteListener<PlaceBufferResponse> mUpdatePlaceDetailsCallback
+            = new OnCompleteListener<PlaceBufferResponse>() {
+        @Override
+        public void onComplete(Task<PlaceBufferResponse> task) {
+            try {
+                PlaceBufferResponse places = task.getResult();
+
+                // Get the Place object from the buffer.
+                Place place = places.get(0);
+
+                // Some example Genres
+                ArrayList<Genre> list = new ArrayList<>();
+                list.add(Genre.ROCK);
+                list.add(Genre.HOUSE);
+
+                // Generate a Test Event
+                event = new Event(1, "Testevent in der gemütlichen Beispielbar",
+                        "Suche talentierten Drehorgelspieler für Freitag Abend in meiner Bar. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+                        list, place, new Timestamp(2019, 1, 20, 20, 15, 0, 0),
+                        new Timestamp(2019, 1, 21, 3, 30, 0, 0), null);
+
+                displayEvent();
+
+
+            } catch (RuntimeRemoteException e) {
+                // Request did not complete successfully
+                Log.e(TAG, "Place query did not complete.", e);
+                return;
+            }
+        }
+    };
+    // endregion
+
+
+    /**
+     * Displays the given Event in the Activity
+     */
+    private void displayEvent() {
         titleText.setText(event.getTitle());
 
         String genreString = "(";
@@ -80,15 +126,17 @@ public class EventProfileActivity extends AppCompatActivity {
         timeText.setText(time);
         String date = event.getTimeFrom().getDate() + "." + event.getTimeFrom().getMonth() + "." + event.getTimeFrom().getYear();
         dateText.setText(date);
-        locationText.setText("TODO");
+        String placeName = event.getLocation().getName().toString();
+        locationText.setText(placeName);
     }
 
     /**
      *  Called when the user clicks anywhere on the Location Icon or Text
      */
     private void onClickLocation() {
-        Log.d(TAG, "Location clicked");
-        // TODO open google map with Location
+        String uriString = "https://www.google.com/maps/search/?api=1&query=Google&query_place_id=" + event.getLocation().getId();
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(uriString));
+        startActivity(browserIntent);
     }
 
 }
