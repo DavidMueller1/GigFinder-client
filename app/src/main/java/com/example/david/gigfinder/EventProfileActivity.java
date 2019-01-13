ea017ad4a9ebc3ddce7e2fe2cad9c018dd6eaab2
HasanTuralic <turalic.ha@gmail.com>
2019-01-13 12:08:51 +0100

    Applying for a Gig works

--- app/src/main/java/com/example/david/gigfinder/EventProfileActivity.java
+++ app/src/main/java/com/example/david/gigfinder/EventProfileActivity.java
@@ -1,6 +1,7 @@
 package com.example.david.gigfinder;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.location.Address;
 import android.location.Geocoder;
@@ -33,7 +34,9 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
@@ -49,6 +52,8 @@ public class EventProfileActivity extends AppCompatActivity {
 
     private Event event;
     private String idToken;
+    private String user;
+    private int userId;
     private JSONObject hostJson;
     private JSONObject eventJson;
 
@@ -61,13 +66,18 @@ public class EventProfileActivity extends AppCompatActivity {
     LinearLayout locationContainer;
 
     Button testBtn;
+    Button applyBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_profile);
 
+        SharedPreferences prefs = getSharedPreferences(getString(R.string.shared_prefs), MODE_PRIVATE);
+
         idToken = getIntent().getExtras().getString("idToken");
+        userId = prefs.getInt("userId", 0);
+        user = prefs.getString("user", "host");
 
         titleText = findViewById(R.id.event_title);
         genreText = findViewById(R.id.event_genre);
@@ -109,6 +119,18 @@ public class EventProfileActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        applyBtn = findViewById(R.id.applyBtn);
+        applyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PostParticipation postParticipation = new PostParticipation();
+                postParticipation.execute();
+            }
+        });
+        if(user.equals("host")){
+            applyBtn.setVisibility(View.GONE);
+        }
     }
 
     /**
@@ -242,4 +264,72 @@ public class EventProfileActivity extends AppCompatActivity {
         }
     }
 
+
+    class PostParticipation extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+                URL url = new URL("https://gigfinder.azurewebsites.net/api/participations");
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+                urlConnection.setRequestProperty("Authorization", idToken);
+                urlConnection.setRequestProperty("Content-Type","application/json");
+                urlConnection.setRequestMethod("POST");
+                urlConnection.setUseCaches(false);
+                urlConnection.setDoOutput(true);
+
+                //Send data
+                DataOutputStream os = new DataOutputStream(urlConnection.getOutputStream());
+                JSONObject jsonObject = new JSONObject();
+                jsonObject.put("EventId", eventJson.getInt("id"));
+                jsonObject.put("ArtistId", userId);
+                os.writeBytes(jsonObject.toString());
+                os.close();
+
+                //Get response
+                InputStream is = null;
+                try {
+                    is = urlConnection.getInputStream();
+                } catch (IOException ioe) {
+                    if (urlConnection instanceof HttpURLConnection) {
+                        HttpURLConnection httpConn = (HttpURLConnection) urlConnection;
+                        int statusCode = httpConn.getResponseCode();
+                        if (statusCode != 200) {
+                            is = httpConn.getErrorStream();
+                            Log.d(TAG, "PostParticipation: STATUS CODE: " + statusCode);
+                            Log.d(TAG, "PostParticipation: RESPONESE MESSAGE: " + httpConn.getResponseMessage());
+                        }
+                    }
+                }
+
+                BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+                StringBuilder response = new StringBuilder();
+                String line;
+                while ((line = rd.readLine()) != null) {
+                    response.append(line);
+                    response.append('\r');
+                }
+                rd.close();
+
+                Log.d(TAG, "PostParticipation: RESPONSE:" + response.toString());
+
+                return response.toString();
+            } catch (ProtocolException e) {
+                e.printStackTrace();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            applyBtn.setClickable(false);
+        }
+    }
 }
